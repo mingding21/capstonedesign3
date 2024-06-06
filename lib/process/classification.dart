5e3cd04fed0bbeb870fa114c70b4a24f8c9cd893
babyd <babydegull@naver.com>
2024-06-06 10:09:27 +0900

    모델 수정

--- lib/process/classification.dart
+++ lib/process/classification.dart
@@ -4,9 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img;
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:capstonedesign3/screen/fishinfo_screen.dart';
-import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' as parser;
-import 'package:html/dom.dart' as dom;
 
 class ClassificationScreen extends StatefulWidget {
   final File? image;
@@ -24,112 +21,48 @@ class _ClassificationScreenState extends State<ClassificationScreen> {
   bool _loading = false;
   late Interpreter _interpreter;
 
-  Map<String, String> classTranslations = {
-    'Indo-Pacific Tarpon': '풀잉어',
-    'Trout': '송어',
-    'Shrimp': '새우',
-    'Perch': '농어',
-    'Mullet': '숭어',
-    'Grass Carp': '초어',
-    'Striped Red Mullet': '줄무늬 붉은 숭어',
-    'Glass Perchlet': '유리잉어',
-    'Hourse Mackerel': '전갱이',
-    'Sea Bass': '배스',
-    'Gold Fish': '금붕어',
-    'Bangus': '갯농어',
-    'Black Spotted Barb': '검은 점 바브',
-    'Pangasius': '판가시우스',
-    'Goby': '망둥이',
-    'Mosquito Fish': '모기고기',
-    'Gilt-Head Bream': '귀족 도미',
-    'Long-Snouted Pipefish': '긴 주둥이 파이브피쉬',
-    'Red Mullet': '붉은 숭어',
-    'Silver Barb': '실버바브',
-    'Tenpounder': '당멸치',
-    'Mudfish': '미꾸라지',
-    'Indian Carp': '인도 잉어',
-    'Big Head Carp': '대두어',
-    'Freshwater Eel': '뱀장어',
-    'Silver Perch': '실버퍼치',
-    'Red Sea Bream': '참돔',
-    'Fourfinger Threadfin': '네날가지',
-    'Knifefish': '칼고기',
-    'Janitor Fish': '',
-    'Catfish': '메기',
-    'Climbing Perch': '등목어',
-    'Snakehead': '가물치',
-    'Gourami': '버들붕어',
-    'Black Sea Sprat': '검정청어',
-    'Jaguar Gapote': '',
-    'Silver Carp': '백련어',
-    'Tilapia': '틸라피아',
-    'Scat Fish': '식분어',
-    'Green Spotted Puffer': '녹색 점박이 복어'
+  Map<int, String> classLabels = {
+    0: 'Bangus (갯농어)',
+    1: 'Freshwater Eel (뱀장어)',
+    2: 'Grass Carp (초어)',
+    3: 'Glass Perchlet (유리잉어)',
+    4: 'Catfish (메기)',
+    5: 'Silver Perch (실버퍼치)',
+    6: 'Gold Fish (금붕어)',
+    7: 'Tenpounder (당멸치)',
+    8: 'Shrimp (새우)',
+    9: 'Goby (망둥이)',
+    10: 'Mudfish (미꾸라지)',
+    11: 'Fourfinger Threadfin (네날가지)',
+    12: 'Janitor Fish',
+    13: 'Knifefish (칼고기)',
+    14: 'Black Spotted Barb (검은 점 바브)',
+    15: 'Big Head Carp (대두어)',
+    16: 'Hourse Mackerel (전갱이)',
+    17: 'Long-Snouted Pipefish (긴 주둥이 파이브피쉬)',
+    18: 'Gilt-Head Bream (귀족 도미)',
+    19: 'Green Spotted Puffer (녹색 점박이 복어)',
+    20: 'Silver Barb (실버바브)',
+    21: 'Silver Carp (백련어)',
+    22: 'Sea Bass (배스)',
+    23: 'Pangasius (판가시우스)',
+    24: 'Red Sea Bream (참돔)',
+    25: 'Mullet (숭어)',
+    26: 'Indian Carp (인도 잉어)',
+    27: 'Gourami (버들붕어)',
+    28: 'Perch (농어)',
+    29: 'Scat Fish (식분어)',
+    30: 'Striped Red Mullet (줄무늬 붉은 숭어)',
+    31: 'Trout (송어)',
+    32: 'Red Mullet (붉은 숭어)',
+    33: 'Indo-Pacific Tarpon (풀잉어)',
+    34: 'Snakehead (가물치)',
+    35: 'Climbing Perch (등목어)',
+    36: 'Jaguar Gapote',
+    37: 'Mosquito Fish (모기고기)',
+    38: 'Black Sea Sprat (검정청어)',
+    39: 'Tilapia (틸라피아)'
   };
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('물고기 분류 결과'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: widget.targetWidth,
-              height: widget.targetHeight,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black), // 여백을 위한 테두리 추가
-              ),
-              child: Image.file(
-                widget.image!,
-                fit: BoxFit.contain, // 이미지를 자르지 않고 여백이 생기도록 설정
-              ),
-            ),
-            SizedBox(height: 20),
-            _loading
-                ? CircularProgressIndicator()
-                : _outputs != null
-                ? Column(
-              children: [
-                if (_outputs![1] < 0.5)
-                  Text(
-                    "오류. 다른 사진을 골라주세요.",
-                    style: TextStyle(fontSize: 20, color: Colors.red),
-                  )
-                else
-                  Column(
-                    children: [
-                      Text(
-                        "분류 결과: ${_outputs![0]} (${classTranslations[_outputs![0]] ?? 'Unknown'})",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text(
-                        "정확도: ${( _outputs![1] * 100).toStringAsFixed(2)}%",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => FishInfoScreen(fishName: _outputs![0])),
-                          );
-                        },
-                        child: Text('물고기 정보'),
-                      ),
-                    ],
-                  )
-              ],
-            )
-                : Container(),
-          ],
-        ),
-      ),
-    );
-  }
 
   @override
   void initState() {
@@ -145,7 +78,7 @@ class _ClassificationScreenState extends State<ClassificationScreen> {
 
     try {
       // 모델 로딩
-      _interpreter = await Interpreter.fromAsset('asset/model/fish_cnn_model_datasetplus.tflite');
+      _interpreter = await Interpreter.fromAsset('asset/model/fish_classification_model_final.tflite');
 
       print('모델 로딩 완료');
       _classifyImage(widget.image!); // 이미지가 여기서 전달되었으므로 따로 선택할 필요 없음
@@ -190,13 +123,14 @@ class _ClassificationScreenState extends State<ClassificationScreen> {
 
     // 예측된 클래스 가져오기
     var predictedClassIndex = output[0].indexOf(output[0].reduce((curr, next) => curr > next ? curr : next));
-    var predictedClassName = classTranslations.keys.toList()[predictedClassIndex];
+
+    // 클래스 이름 가져오기
+    var predictedClassName = classLabels[predictedClassIndex];
 
     // 정확도 계산
     var confidence = output[0][predictedClassIndex];
-    var accuracy = confidence / output[0].reduce((curr, next) => curr + next);
 
-    return [predictedClassName, accuracy];
+    return [predictedClassName, confidence];
   }
 
   Future<Uint8List> _preprocessImage(File image) async {
@@ -214,6 +148,70 @@ class _ClassificationScreenState extends State<ClassificationScreen> {
     }
 
     return floatList.buffer.asUint8List();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('물고기 분류 결과'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: widget.targetWidth,
+              height: widget.targetHeight,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black), // 여백을 위한 테두리 추가
+              ),
+              child: Image.file(
+                widget.image!,
+                fit: BoxFit.contain, // 이미지를 자르지 않고 여백이 생기도록 설정
+              ),
+            ),
+            SizedBox(height: 20),
+            _loading
+                ? CircularProgressIndicator()
+                : _outputs != null
+                ? Column(
+              children: [
+                if (_outputs![1] < 0.5)
+                  Text(
+                    "오류. 다른 사진을 골라주세요.",
+                    style: TextStyle(fontSize: 20, color: Colors.red),
+                  )
+                else
+                  Column(
+                    children: [
+                      Text(
+                        "분류 결과: ${_outputs![0]}",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        "정확도: ${( _outputs![1] * 100).toStringAsFixed(2)}%",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => FishInfoScreen(fishName: _outputs![0].split(' (')[0])),
+                          );
+                        },
+                        child: Text('물고기 정보'),
+                      ),
+                    ],
+                  )
+              ],
+            )
+                : Container(),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
